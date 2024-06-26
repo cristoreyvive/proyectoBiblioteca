bbcc42a44f75ff04b22e07db3e106c26669f791d
cristoreyvive <cristo.31670264@uru.edu>
2024-06-26 02:00:08 -0400

    add: users functions, login logic completed

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,27 @@
 
 using namespace std;
 
-void login();
+bool login();
 void agregarLibro();
 void modificarLibro();
 void eliminarLibro();
+void agregarUsuario();
+void eliminarUsuario();
+void suspenderUsuario();
+void quitarSuspension();
 
 int main()
 {
-    login();
+    if (!login())
+    {
+        cout << "Inicio de sesión fallido. Saliendo del sistema.\n";
+        return 1;
+    }
 
     int opcion;
     do
     {
-        cout << "1. Agregar libro\n2. Modificar libro\n3. Eliminar libro\n4. Salir\n";
+        cout << "1. Agregar libro\n2. Modificar libro\n3. Eliminar libro\n4. Agregar usuario\n5. Eliminar usuario\n6. Suspender usuario\n7. Quitar suspensión\n8. Salir\n";
         cout << "Seleccione una opción: ";
         cin >> opcion;
 
@@ -32,42 +40,77 @@ int main()
             eliminarLibro();
             break;
         case 4:
+            agregarUsuario();
+            break;
+        case 5:
+            eliminarUsuario();
+            break;
+        case 6:
+            suspenderUsuario();
+            break;
+        case 7:
+            quitarSuspension();
+            break;
+        case 8:
             cout << "Saliendo del sistema...\n";
             break;
         default:
             cout << "Opción no válida.\n";
         }
-    } while (opcion != 4);
+    } while (opcion != 8);
 
     return 0;
 }
 
-void login()
+bool login()
 {
-    string username, password, stored_username, stored_password;
+    string username, password, stored_username, stored_password, status;
     cout << "Ingrese su nombre de usuario: ";
     cin >> username;
     cout << "Ingrese su contraseña: ";
     cin >> password;
 
-    ifstream usersFile("bin/users.csv");
+    ifstream usersFile("users.csv");
     if (usersFile.is_open())
     {
-        while (usersFile >> stored_username >> stored_password)
+        bool encontrado = false;
+        while (getline(usersFile, stored_username, ',') && getline(usersFile, stored_password, ',') && getline(usersFile, status))
         {
+            stored_username.erase(0, stored_username.find_first_not_of(" \t"));
+            stored_username.erase(stored_username.find_last_not_of(" \t") + 1);
+            stored_password.erase(0, stored_password.find_first_not_of(" \t"));
+            stored_password.erase(stored_password.find_last_not_of(" \t") + 1);
+            status.erase(0, status.find_first_not_of(" \t"));
+            status.erase(status.find_last_not_of(" \t") + 1);
+
             if (username == stored_username && password == stored_password)
             {
-                cout << "Inicio de sesión exitoso.\n";
-                usersFile.close();
-                return;
+                if (status == "suspendido")
+                {
+                    cout << "Su cuenta está suspendida. Acceso denegado.\n";
+                    return false;
+                }
+                encontrado = true;
+                break;
             }
         }
-        cout << "Nombre de usuario o contraseña incorrectos.\n";
         usersFile.close();
+
+        if (encontrado)
+        {
+            cout << "Inicio de sesión exitoso.\n";
+            return true;
+        }
+        else
+        {
+            cout << "Nombre de usuario o contraseña incorrectos.\n";
+            return false;
+        }
     }
     else
     {
         cout << "No se pudo abrir el archivo de usuarios.\n";
+        return false;
     }
 }
 
@@ -80,7 +123,7 @@ void agregarLibro()
     cout << "Ingrese el autor del libro: ";
     getline(cin, autor);
 
-    ofstream librosFile("bin/libros.csv", ios::app);
+    ofstream librosFile("libros.csv", ios::app);
     if (librosFile.is_open())
     {
         librosFile << titulo << "," << autor << "\n";
@@ -105,8 +148,8 @@ void modificarLibro()
     cout << "Ingrese el nuevo autor del libro: ";
     getline(cin, nuevoAutor);
 
-    ifstream archivoEntrada("bin/libros.csv");
-    ofstream archivoTemporal("bin/libros_temp.csv");
+    ifstream archivoEntrada("libros.csv");
+    ofstream archivoTemporal("libros_temp.csv");
 
     if (archivoEntrada.is_open() && archivoTemporal.is_open())
     {
@@ -130,8 +173,8 @@ void modificarLibro()
         archivoEntrada.close();
         archivoTemporal.close();
 
-        remove("bin/libros.csv");
-        rename("bin/libros_temp.csv", "bin/libros.csv");
+        remove("libros.csv");
+        rename("libros_temp.csv", "libros.csv");
 
         if (encontrado)
         {
@@ -155,8 +198,8 @@ void eliminarLibro()
     cin.ignore();
     getline(cin, titulo);
 
-    ifstream archivoEntrada("bin/libros.csv");
-    ofstream archivoTemporal("bin/libros_temp.csv");
+    ifstream archivoEntrada("libros.csv");
+    ofstream archivoTemporal("libros_temp.csv");
 
     if (archivoEntrada.is_open() && archivoTemporal.is_open())
     {
@@ -178,8 +221,8 @@ void eliminarLibro()
         archivoEntrada.close();
         archivoTemporal.close();
 
-        remove("bin/libros.csv");
-        rename("bin/libros_temp.csv", "bin/libros.csv");
+        remove("libros.csv");
+        rename("libros_temp.csv", "libros.csv");
 
         if (encontrado)
         {
@@ -193,5 +236,180 @@ void eliminarLibro()
     else
     {
         cout << "No se pudo abrir el archivo de libros.\n";
+    }
+}
+
+void agregarUsuario()
+{
+    string username, password;
+    cout << "Ingrese el nombre de usuario: ";
+    cin >> username;
+    cout << "Ingrese la contraseña: ";
+    cin >> password;
+
+    ofstream usersFile("users.csv", ios::app);
+    if (usersFile.is_open())
+    {
+        usersFile << username << "," << password << "\n";
+        usersFile.close();
+        cout << "Usuario agregado exitosamente.\n";
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo de usuarios.\n";
+    }
+}
+
+void eliminarUsuario()
+{
+    string username, linea;
+    cout << "Ingrese el nombre de usuario a eliminar: ";
+    cin.ignore();
+    getline(cin, username);
+
+    ifstream archivoEntrada("users.csv");
+    ofstream archivoTemporal("users_temp.csv");
+
+    if (archivoEntrada.is_open() && archivoTemporal.is_open())
+    {
+        bool encontrado = false;
+        while (getline(archivoEntrada, linea))
+        {
+            size_t comaPos = linea.find(',');
+            string usernameActual = linea.substr(0, comaPos);
+
+            if (usernameActual == username)
+            {
+                encontrado = true;
+            }
+            else
+            {
+                archivoTemporal << linea << "\n";
+            }
+        }
+        archivoEntrada.close();
+        archivoTemporal.close();
+
+        remove("users.csv");
+        rename("users_temp.csv", "users.csv");
+
+        if (encontrado)
+        {
+            cout << "Usuario eliminado exitosamente.\n";
+        }
+        else
+        {
+            cout << "Usuario no encontrado.\n";
+        }
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo de usuarios.\n";
+    }
+}
+
+void suspenderUsuario()
+{
+    string username, linea;
+    cout << "Ingrese el nombre de usuario a suspender: ";
+    cin.ignore();
+    getline(cin, username);
+
+    ifstream archivoEntrada("users.csv");
+    ofstream archivoTemporal("users_temp.csv");
+
+    if (archivoEntrada.is_open() && archivoTemporal.is_open())
+    {
+        bool encontrado = false;
+        while (getline(archivoEntrada, linea))
+        {
+            size_t comaPos = linea.find(',');
+            string usernameActual = linea.substr(0, comaPos);
+            string resto = linea.substr(comaPos + 1);
+            size_t comaPos2 = resto.find(',');
+            string passwordActual = resto.substr(0, comaPos2);
+            string estado = resto.substr(comaPos2 + 1);
+
+            if (usernameActual == username)
+            {
+                archivoTemporal << usernameActual << "," << passwordActual << ",suspendido\n";
+                encontrado = true;
+            }
+            else
+            {
+                archivoTemporal << linea << "\n";
+            }
+        }
+        archivoEntrada.close();
+        archivoTemporal.close();
+
+        remove("users.csv");
+        rename("users_temp.csv", "users.csv");
+
+        if (encontrado)
+        {
+            cout << "Usuario suspendido exitosamente.\n";
+        }
+        else
+        {
+            cout << "Usuario no encontrado.\n";
+        }
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo de usuarios.\n";
+    }
+}
+
+void quitarSuspension()
+{
+    string username, linea;
+    cout << "Ingrese el nombre de usuario al que desea quitarle la suspensión: ";
+    cin.ignore();
+    getline(cin, username);
+
+    ifstream archivoEntrada("users.csv");
+    ofstream archivoTemporal("users_temp.csv");
+
+    if (archivoEntrada.is_open() && archivoTemporal.is_open())
+    {
+        bool encontrado = false;
+        while (getline(archivoEntrada, linea))
+        {
+            size_t comaPos = linea.find(',');
+            string usernameActual = linea.substr(0, comaPos);
+            string resto = linea.substr(comaPos + 1);
+            size_t comaPos2 = resto.find(',');
+            string passwordActual = resto.substr(0, comaPos2);
+            string estado = resto.substr(comaPos2 + 1);
+
+            if (usernameActual == username)
+            {
+                archivoTemporal << usernameActual << "," << passwordActual << ",activo\n";
+                encontrado = true;
+            }
+            else
+            {
+                archivoTemporal << linea << "\n";
+            }
+        }
+        archivoEntrada.close();
+        archivoTemporal.close();
+
+        remove("users.csv");
+        rename("users_temp.csv", "users.csv");
+
+        if (encontrado)
+        {
+            cout << "Se ha quitado la suspensión al usuario exitosamente.\n";
+        }
+        else
+        {
+            cout << "Usuario no encontrado.\n";
+        }
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo de usuarios.\n";
     }
 }
